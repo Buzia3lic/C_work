279763ce72b1b49c873c1818070bdedfe7ce838d
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-05 22:19:19 +0300

    add if

---
+++ if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("UraAAA!!!");
+}
+else
+{
+    Console.Write("Hello ");
+    Console.Write(username);
+}
