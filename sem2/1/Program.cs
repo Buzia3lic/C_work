7b9ca0565a3f6efa871bf187f342573595e1e051
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-13 16:28:40 +0300

    2 sem

---
+++ sem2/1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+
+
+int num = new Random().Next(10, 100);
+int a1 = num % 10;
+int a2 = num / 10;
+
+int max = a1;
+if (a2 > max)
+{
+    max = a2;
+}
+
+Console.Write($"Max number is {max} from {num}");
