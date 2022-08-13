5bd65417adc2caaefe6fa466d8eaa5132a69f6da
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-13 16:37:01 +0300

    2 sem_2

---
+++ sem2/2/Program.cs
@@ -0,0 +1,7 @@
+﻿int num = new Random().Next(100, 1000);
+int a1 = num / 100;
+int a2 = (num % 100)/10;
+int a3 = (num % 100)%10;
+int res = a1*10 + a3;
+Console.WriteLine($"The number is {num} and res is: {a1}, {a2}, {a3}");
+Console.Write($"The result number is: {res}");
