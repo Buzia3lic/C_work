e3d79bf1df93dbadc8372f787627b58aea93a51c
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-17 17:34:02 +0300

    lec3 end

--- lec3.ex1/Program.cs
+++ lec3.ex1/Program.cs
@@ -22,17 +22,85 @@
 // Console.WriteLine(year);
 
 
-string Method4(int count, string text)
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while(i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "qwerty ");
+// Console.WriteLine(res);
+
+// string Method41(int count, string text)
+// {
+//     string result = String.Empty;
+//     for(int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+
+// string res = Method41(5, "qwerty ");
+// Console.WriteLine(res);
+
+//string text = "-Я просто пишу какой-то текст, чтобы проверить работу программы";
+//string s = "qwerty"
+//            01234
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+
+//     int length = text.Length;
+//     for(int i = 0; i < length; i++)
+//     {
+//         if(text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+//     return result;
+// }
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 7 };
+
+void PrintArr(int[] array)
 {
-    int i = 0;
-    string result = String.Empty;
-    while(i < count)
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
     {
-        result = result + text;
-        i++;
+        Console.Write($"{array[i]} ");
     }
-    return result;
+    Console.WriteLine();
 }
 
-string res = Method4(10, "qwerty ");
-Console.WriteLine(res);
+void Selection(int[] array)
+{
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPos = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPos]) minPos = j;
+        }
+
+        int temp = array[i];
+        array[i] = array[minPos];
+        array[minPos] = temp;
+    }
+}
+
+
+PrintArr(arr);
+Selection(arr);
+PrintArr(arr);
