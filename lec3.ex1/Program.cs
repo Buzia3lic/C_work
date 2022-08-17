dc111f3156ccc9a42f8596ab8ad4571b958400ce
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-17 16:41:01 +0300

    lec3

---
+++ lec3.ex1/Program.cs
@@ -0,0 +1,38 @@
+﻿// void Method1()
+// {
+//     Console.WriteLine("Author...");
+// }
+
+// Method1();
+
+
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+
+// Method2(msg: "text message...");
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+
+// int year = Method3();
+// Console.WriteLine(year);
+
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while(i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "qwerty ");
+Console.WriteLine(res);
